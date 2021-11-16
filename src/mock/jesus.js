169458b16216c0cb63e6c4351b2ed905d4874a9d
Jesus Camacaro <jesus.alejandr@gmail.com>
2021-11-16 12:12:16 -0300

    init

---
+++ src/mock/jesus.js
@@ -0,0 +1,29 @@
+
+const paths = {
+  "1": {
+    to: 1,
+    from: 2,
+    averageTime: '2h',
+    deadline: '12:00',
+    pickeo: '1h'
+  },
+  "2": {
+    to: 2,
+    from: 3,
+    averageTime: '1h',
+    deadline: '9:00',
+    pickeo: '2h',
+    exitleg: '10:30'
+  }
+}
+
+const package = {
+  paht: [1, 2],
+  errors: {
+    "1": {
+      averageTime: '3h',
+      deadline: '7:00',
+      pickeo: '30m'
+    }
+  }
+}
